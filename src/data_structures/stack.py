eb5f4f33a98d83a11ffc7153a3bcbdd7e29afada
Yarin Gaida <yarin.gaida@gmail.com>
2026-02-14 20:49:30 +0200

    stack implementation update

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -7,33 +7,24 @@ class Stack:
         self.items = [] # "items" will hold all elements of the stack
 
     def push(self, value):
-        """
-        Adds an element to the top of the stack.
-        Time Complexity: O(1)
-        """
+        """ Adds an element to the top of the stack. """
         self.items.append(value)
 
     def pop(self):
-        """
-        Removes and returns the top element.
-        Returns None if the stack is empty.
-        """
+        """ Removes and returns the top element. """
         if self.is_empty(): # calls to is_empty
             print("Stack is empty!")
             return None
         return self.items.pop()
 
     def peek(self):
-        """
-        Returns the top element without removing it.
-        Useful for inspecting the stack's state.
-        """
+        """ Returns the top element without removing it. """
         if self.is_empty():
             return None
         return self.items[-1]
 
     def is_empty(self):
-        """Returns True if the stack contains no elements."""
+        """ Returns True if the stack contains no elements."""
         return len(self.items) == 0 # True if empty, False if not
 
     def size(self):
