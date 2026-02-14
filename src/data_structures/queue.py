33b16d9f92b6f8be26759c00498aac4a2dba2c24
Yarin Gaida <yarin.gaida@gmail.com>
2026-02-14 20:37:19 +0200

    update queue code

--- src/data_structures/queue.py
+++ src/data_structures/queue.py
@@ -2,22 +2,23 @@ from collections import deque
 
 class Queue:
     def __init__(self):
-        # I used 'deque' because popping from the start of a standard list is slow (O(n)).
-        # deque allows O(1) for adding/removing from both ends.
+        """ I used 'deque' because popping from the start of a standard list is "expensive" (O(n)).
+        In a regular list, removing the first element forces every other item to shift one position left.
+        'deque' handles this in O(1) by just "unlinking" the head, making the queue much faster. """
         self.items = deque()
 
     def enqueue(self, value):
-        """Add an element to the end of the queue."""
+        # Add an element to the end of the queue.
         self.items.append(value)
 
     def dequeue(self):
-        """Remove the first element from the queue (FIFO)."""
+        # Remove the first element from the queue (FIFO).
         if self.is_empty():
             return None
         return self.items.popleft() # 'popleft' is optimized for queues
 
     def peek(self):
-        """Look at the front element without removing it."""
+        # Look at the front element without removing it.
         if self.is_empty():
             return None
         return self.items[0] # Access the first element
